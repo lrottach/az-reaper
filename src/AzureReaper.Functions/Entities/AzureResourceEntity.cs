a1f1faf497b7cc22984e6696ede13f09ae21742f
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-04-02 16:07:43 +0000

    feat: introduced Azure resource entity

---
+++ src/AzureReaper.Functions/Entities/AzureResourceEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Worker.Extensions.DurableTask;
+using Microsoft.DurableTask;
+
+namespace AzureReaper.Function.Entities;
+
+
+[JsonObject(MemberSerialization.OptIn)]
+public class AzureResourceEntity
+{
+    [JsonProperty("value")]
+    public int CurrentValue { get; set; }
+
+    public void Add(int amount) => this.CurrentValue += amount;
+
+    public void Reset() => this.CurrentValue = 0;
+
+    public int Get() => this.CurrentValue;
+
+    [Function(nameof(AzureResourceEntity))]
+    public static Task RunEntityAsync([EntityTrigger] TaskEntityDispatcher dispatcher)
+    {
+        return dispatcher.DispatchAsync<AzureResourceEntity>();
+    }
+}
