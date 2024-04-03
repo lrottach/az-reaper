da21134041001113c5df80d2c2add85fef7154ad
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-04-03 21:29:32 +0000

    feat: prepared eventgrid trigger logic

--- src/AzureReaper.Functions/EventGridTrigger.cs
+++ src/AzureReaper.Functions/EventGridTrigger.cs
@@ -5,6 +5,12 @@ using System;
 using Azure.Messaging;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.DurableTask.Client;
+using DurableTask.Core.Entities;
+using AzureReaper.Function.Entities;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Entities;
+using Microsoft.DurableTask.Client.Entities;
 
 namespace AzureReaper.Function
 {
@@ -18,9 +24,32 @@ namespace AzureReaper.Function
         }
 
         [Function(nameof(EventGridTrigger))]
-        public void Run([EventGridTrigger] CloudEvent cloudEvent)
+        public async Task Run([EventGridTrigger] CloudEvent cloudEvent,
+        [DurableClient] DurableTaskClient client)
         {
             _logger.LogInformation("Event type: {type}, Event subject: {subject}", cloudEvent.Type, cloudEvent.Subject);
+
+            // Make sure function will only continue for the correct event types
+            if (cloudEvent.Type != "Microsoft.Resources.ResourceWriteSuccess")
+            {
+                _logger.LogInformation("You shall not pass: {EventType}", cloudEvent.Type);
+                return;
+            }
+
+            // Create new entity for current event
+            EntityInstanceId entityId = new EntityInstanceId(nameof(AzureResourceEntity), cloudEvent.Subject.Replace("/", ""));
+
+            // Read entities state to process later
+            EntityMetadata<AzureResourceEntity>? entityState = await client.Entities.GetEntityAsync<AzureResourceEntity>(entityId);
+
+            // If entity exists and is scheduled, nothing to do
+            if (entityState != null || entityState.State.Scheduled)
+            {
+                _logger.LogWarning("Entity for Resource Id '{ResourceId}' already exists and death was already scheduled", entityState.State.ResourceId);
+            }
+
+            // Initialize entity
+            await client.Entities.SignalEntityAsync(entityId, "InitializeEntity");
         }
     }
 }
