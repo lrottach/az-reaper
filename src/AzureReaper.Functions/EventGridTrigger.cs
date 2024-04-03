1841bb9531e1d32949cdfda5dadb897c4f22b888
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-04-03 20:38:24 +0000

    feat: introduced event grid trigger

---
+++ src/AzureReaper.Functions/EventGridTrigger.cs
@@ -0,0 +1,26 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using System;
+using Azure.Messaging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureReaper.Function
+{
+    public class EventGridTrigger
+    {
+        private readonly ILogger<EventGridTrigger> _logger;
+
+        public EventGridTrigger(ILogger<EventGridTrigger> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(EventGridTrigger))]
+        public void Run([EventGridTrigger] CloudEvent cloudEvent)
+        {
+            _logger.LogInformation("Event type: {type}, Event subject: {subject}", cloudEvent.Type, cloudEvent.Subject);
+        }
+    }
+}
