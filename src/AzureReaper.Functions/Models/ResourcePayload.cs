4d98e702315dd942dc8bb624a2c12c8e505ecdef
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-04-15 14:58:12 +0000

    feat: added new class to represent resource payload required for initializing #36

---
+++ src/AzureReaper.Functions/Models/ResourcePayload.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureReaper.Function.Models;
+
+// Represents the information required to initialize a new AzureResourceEntity
+public class ResourcePayload
+{
+    public required string SubscriptionId { get; set; }
+    public string? ResourceId { get; set; }
+    public required string ResourceGroup { get; set; }
+}
