9039550bf230b65eb5e84d09e43386a09e7a9a01
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-04-15 15:00:04 +0000

    feat: introduced string handler to extract information from #35

---
+++ src/AzureReaper.Functions/Common/StringHandler.cs
@@ -0,0 +1,18 @@
+﻿using AzureReaper.Function.Models;
+
+namespace AzureReaper.Function.Common;
+
+public static class StringHandler
+{
+    // Extract required information from an Azure resource Id and return a ResourcePayload object
+    public static ResourcePayload ExtractResourcePayload(string resourceId)
+    {
+        string[] parts = resourceId.Split("/");
+        return new ResourcePayload
+        {
+            SubscriptionId = parts[2],
+            ResourceId = resourceId,
+            ResourceGroup = parts[4]
+        };
+    }
+}
