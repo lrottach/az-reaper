55bd8b8b3073d50c4742c1c8a4b6a2400b524623
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-05-29 18:36:13 +0200

    refactor: moved EventGridTrigger.cs to file-scoped namespace

--- src/AzureReaper.Functions/EventGridTrigger.cs
+++ src/AzureReaper.Functions/EventGridTrigger.cs
@@ -10,26 +10,26 @@ using Microsoft.DurableTask.Entities;
 using Microsoft.Extensions.Logging;
 using AzureReaper.Models;
 
-namespace AzureReaper
+namespace AzureReaper;
+
+public class EventGridTrigger(ILogger<EventGridTrigger> logger)
 {
-    public class EventGridTrigger(ILogger<EventGridTrigger> logger)
-    {
-        [Function(nameof(EventGridTrigger))]
-        public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent,
+    [Function(nameof(EventGridTrigger))]
+    public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent,
         [DurableClient] DurableTaskClient client)
+    {
+        logger.LogInformation("[EventGridTrigger] Event type: {type}, Event subject: {subject}", eventGridEvent.EventType, eventGridEvent.Subject);
+
+        // Make sure function will only continue for the correct event types
+        // Additional validation in case, EventGrid filter wasn't configured correctly
+        if (eventGridEvent.EventType != "Microsoft.Resources.ResourceWriteSuccess")
         {
-            logger.LogInformation("[EventGridTrigger] Event type: {type}, Event subject: {subject}", eventGridEvent.EventType, eventGridEvent.Subject);
+            logger.LogInformation("[EventGridTrigger] You shall not pass: {EventType}", eventGridEvent.EventType);
+            return;
+        }
 
-            // Make sure function will only continue for the correct event types
-            // Additional validation in case, EventGrid filter wasn't configured correctly
-            if (eventGridEvent.EventType != "Microsoft.Resources.ResourceWriteSuccess")
-            {
-                logger.LogInformation("[EventGridTrigger] You shall not pass: {EventType}", eventGridEvent.EventType);
-                return;
-            }
-
-            // Create new entity for current event
-            EntityInstanceId entityId = new EntityInstanceId(nameof(AzureResourceEntity), eventGridEvent.Subject.Replace("/", ""));
+        // Create new entity for current event
+        EntityInstanceId entityId = new EntityInstanceId(nameof(AzureResourceEntity), eventGridEvent.Subject.Replace("/", ""));
             
         // Build resource payload from event subject
         ResourcePayload resourcePayload = StringHandler.ExtractResourcePayload(eventGridEvent.Subject);
